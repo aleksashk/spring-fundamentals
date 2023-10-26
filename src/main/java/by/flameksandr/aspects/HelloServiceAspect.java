2e271ed69200b6f616a787056b8e19460b98c663
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 19:57:03 +0300

    @After

--- src/main/java/by/flameksandr/aspects/HelloServiceAspect.java
+++ src/main/java/by/flameksandr/aspects/HelloServiceAspect.java
@@ -1,5 +1,6 @@
 package by.flameksandr.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,11 @@ import org.springframework.stereotype.Component;
 public class HelloServiceAspect {
     @Before("execution(* by.flameksandr.services.HelloService.hello(..))")
     public void before() {
+        System.out.println("A");
+    }
+
+    @After("execution(* by.flameksandr.services.HelloService.hello(..))")
+    public void after() {
         System.out.println("A");
     }
 
